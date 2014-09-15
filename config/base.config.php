10a9881c1b644c8f8d786620a98c3185ba3fe962
Carlos Lizaga <admin@karldeux.com>
2014-09-15 13:04:35 +0200

    Update base.config.php

--- config/base.config.php
+++ config/base.config.php
@@ -1,19 +1,19 @@
 <?php
 
-define('__BASE_URI__', 'http://www.neurotix.es/sandbox/');
-define('__NEURO_BASE_URI__', getenv("DOCUMENT_ROOT") . "/sandbox/");
-define('__NEURO_ADMIN_URI__', getenv("DOCUMENT_ROOT") . "/sandbox/admin/");
-define('__SITE_TITLE__', 'Alejandro Albarracín');
-define('__SITE_DESCRIPTION__', 'Alejandro Albarración - Página WEB personal - Modelo - Actor');
+define('__BASE_URI__', 'base_uri');
+define('__NEURO_BASE_URI__', getenv("DOCUMENT_ROOT"));
+define('__NEURO_ADMIN_URI__', getenv("DOCUMENT_ROOT") . "/admin/");
+define('__SITE_TITLE__', 'Site title');
+define('__SITE_DESCRIPTION__', 'Site description');
 define('_IMG_UPLOADS_', __NEURO_BASE_URI__ . "/uploads/img/");
 define('_ADMIN_NAME_', 'admin');
 define('_THEME_NAME_', 'basic');
 define('_DB_NAME_', 'neurocms');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_DB_SERVER_', 'localhost');
-define('_DB_USER_', 'neurotix');
+define('_DB_USER_', 'user');
 define('_DB_PREFIX_', 'neuro_');
-define('_DB_PASSWD_', 'z43ngfBm');
+define('_DB_PASSWD_', 'password');
 define('_DB_TYPE_', 'MySQL');
 define('_UID_', 'c8bc3d0e-e6b5-afac-6693-536fa323c25b');
 define('_FRIENDLY_URL_', 'true');
